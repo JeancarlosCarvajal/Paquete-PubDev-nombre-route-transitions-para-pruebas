1b8918f622de36facf69f0c0bb5593b341fdcaae
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-08-19 15:58:16 -0400

    Configurado las configuraciones iniciales del paquete

--- lib/route_transitions.dart
+++ lib/route_transitions.dart
@@ -1,7 +1,73 @@
-library route_transitions;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+
+enum AnimationType {
+  normal,
+  fadeIn
+} 
+
+class RouteTransitions {
+  final BuildContext context;
+  final Widget child;
+  final Duration duration;
+  final AnimationType animation;
+  final bool replacement;
+
+  RouteTransitions({
+    required this.context, 
+    required this.child,
+    duration, 
+    animation,
+    replacement
+  }): animation = animation ?? AnimationType.normal,
+      duration = duration ?? const Duration(milliseconds: 300),
+      replacement = replacement ?? false 
+  {
+
+    // hacemos un switch
+    switch( this.animation ){
+      case AnimationType.normal: 
+        this._normalTrasition();
+        break;
+      case AnimationType.fadeIn:
+        this._fadeInTransition();
+        break;
+    } 
+
+
+  }
+
+  void _pushPage( Route route ) => Navigator.push(context, route);
+  void _pushReplacementPage( Route route ) => Navigator.pushReplacement(context, route);
+
+  void _normalTrasition() {
+    final route = MaterialPageRoute(builder: (_) => child); 
+    // verifica que sea replacement o no
+    replacement 
+      ? _pushReplacementPage(route)
+      : _pushPage(route); 
+  }
+
+  void _fadeInTransition() {
+
+    final route = PageRouteBuilder(
+      pageBuilder: ( _ , __ , ___ ) => child,
+      transitionDuration: duration,
+      transitionsBuilder: ( _ , animation, __ , child ) {
+        return FadeTransition(
+          opacity: Tween<double>(begin: 0.0, end: 1.0).animate(
+            CurvedAnimation( parent: animation, curve: Curves.easeOut )
+          ),
+          child: child
+        );
+      }
+    ); 
+    
+    // verifica que sea replacement o no
+    replacement 
+      ? _pushReplacementPage(route)
+      : _pushPage(route);
+
+  }
+
 }
